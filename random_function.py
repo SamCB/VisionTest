1c51938126cd9f113da234f75dcec2d2358fc2e4
Sam Collis-Bird <s.cbird01@gmail.com>
2016-09-06 19:16:28 +1000

    Initial Commit

---
+++ random_function.py
@@ -0,0 +1,81 @@
+import utils
+import random
+
+
+def use():
+    return RandomFunction().answer
+
+
+def random_rect(world_height, world_width, x=None, y=None, width=None, height=None):
+    if x is None:
+        return {
+            'x': random.randrange(0, world_width),
+            'y': random.randrange(0, world_height),
+            'width': random.randrange(10, 100),
+            'height': random.randrange(10, 100)
+        }
+    else:
+        return {
+            'x': random.randrange(max(0, x - 10), min(world_width, x + 10) + 1),
+            'y': random.randrange(max(0, y - 10), min(world_height, y + 10) + 1),
+            'width': random.randrange(width - 2, width + 2 + 1),
+            'height': random.randrange(height - 2, height + 2 + 1)
+        }
+
+
+def random_line(world_height, world_width, x1=None, y1=None, x2=None, y2=None):
+    if x1 is None:
+        return {
+            'x1': random.randrange(0, world_width),
+            'y1': random.randrange(0, world_height),
+            'x2': random.randrange(0, world_width),
+            'y2': random.randrange(0, world_height)
+        }
+    else:
+        return {
+            'x1': random.randrange(max(0, x1 - 2),
+                                   min(world_width, x1 + 2) + 1),
+            'y1': random.randrange(max(0, y1 - 2),
+                                   min(world_height, y1 + 2) + 1),
+            'x2': random.randrange(max(0, x2 - 2),
+                                   min(world_width, x2 + 2) + 1),
+            'y2': random.randrange(max(0, y2 - 2),
+                                   min(world_height, y2 + 2) + 1),
+        }
+
+
+def random_point(world_height, world_width, x=None, y=None):
+    if x is None:
+        return {
+            'x': random.randrange(0, world_width),
+            'y': random.randrange(0, world_height)
+        }
+    else:
+        return {
+            'x': random.randrange(max(0, x - 2), min(world_width, x + 2) + 1),
+            'y': random.randrange(max(0, y - 2), min(world_height, y + 2) + 1)
+        }
+
+
+class RandomFunction:
+
+    def __init__(self):
+        self.features = []
+        for feature in utils.USED_FEATURES:
+            f = {"schema": feature.schema, "name": feature.name}
+            if feature.schema == utils.Rectangle:
+                f['func'] = random_rect
+            elif feature.schema == utils.Line:
+                f['func'] = random_line
+            elif feature.schema == utils.Point:
+                f['func'] = random_point
+            f['points'] = f['func'](720, 1280)
+            self.features.append(f)
+
+    def answer(self, frame):
+        output = []
+        for f in self.features:
+            f['points'] = f['func'](*frame.shape[:2], **f['points'])
+            output.append((f['name'], f['points']))
+
+        return output
---
+++ utils.py
@@ -0,0 +1,130 @@
+import cv2
+import numpy as np
+
+from collections import Counter
+
+FONT = cv2.FONT_HERSHEY_PLAIN
+COLOUR = (0, 0, 255)
+LINE_WIDTH = 1
+
+
+class Rectangle:
+    def __init__(self, x, y, width, height, name=None):
+        self.x = np.float32(x)
+        self.y = np.float32(y)
+        self.height = np.float32(height)
+        self.width = np.float32(width)
+        self.name = name
+
+    def draw(self, img, colour=COLOUR):
+        cv2.rectangle(img,
+                      (self.x, self.y),
+                      (self.x + self.width, self.y + self.height),
+                      colour,
+                      LINE_WIDTH)
+        if self.name:
+            title = "{}: x:{:.1f} y:{:.1f} w:{:.1f} h:{:.1f}".format(
+                self.name, self.x, self.y, self.width, self.height
+            )
+        else:
+            title = "x:{:.1f} y:{:.1f} w:{:.1f} h:{:.1f}".format(
+                self.x, self.y, self.width, self.height
+            )
+        cv2.putText(img, title,
+                    (self.x, self.y - np.float32(2)),
+                    FONT, 0.5, colour, 1)
+
+    def __repr__(self):
+        return "{}({}, {}, {}, {}, {})".format(self.__class__.__name__,
+                                               self.x, self.y,
+                                               self.width, self.height,
+                                               self.name)
+
+
+class Point:
+
+    CIRCLE_RADIUS = 3
+
+    def __init__(self, x, y, name=None):
+        self.x = np.float32(x)
+        self.y = np.float32(y)
+        self.name = name
+
+    def draw(self, img, colour=COLOUR):
+        cv2.circle(img, (self.x, self.y), Point.CIRCLE_RADIUS, colour, LINE_WIDTH)
+        if self.name:
+            title = "{}: x:{:.1f} y:{:.1f}".format(self.name, self.x, self.y)
+        else:
+            title = "x:{:.1f} y:{:.1f}".format(self.x, self.y)
+        cv2.putText(img, title,
+                    (self.x, self.y - np.float32(Point.CIRCLE_RADIUS + 1)),
+                    FONT, 0.5, colour, 1)
+    def __repr__(self):
+        return "{}({}, {}, {})".format(self.__class__.__name__,
+                                       self.x, self.y, self.name)
+
+
+class Line:
+    def __init__(self, x1, y1, x2, y2, name=None):
+        self.x1 = np.float32(x1)
+        self.y1 = np.float32(y1)
+        self.x2 = np.float32(x2)
+        self.y2 = np.float32(y2)
+        self.name = name
+
+    def draw(self, img, colour=COLOUR):
+        cv2.line(img, (self.x1, self.y1), (self.x2, self.y2), colour, LINE_WIDTH)
+        if self.name:
+            title = "{}: x1:{:.1f} y1:{:.1f} x2:{:.1f} y2:{:.1f}".format(
+                self.name, self.x1, self.y1, self.x2, self.y2
+            )
+        else:
+            title = "x1:{:.1f} y1:{:.1f} x2:{:.1f} y2:{:.1f}".format(
+                self.x1, self.y1, self.x2, self.y2
+            )
+        if self.y1 >= self.y2:
+            # slope down, text should be above
+            pos = (self.x1, self.y1 - np.float32(1))
+        else:
+            # slope up, text should be below
+            pos = (self.x1, self.y1 + np.float32(6))
+
+    def __repr__(self):
+        return "{}({}, {}, {}, {})".format(self.__class__.__name__,
+                                           self.x1, self.y1,
+                                           self.x2, self.y2)
+
+
+def from_dictionary(dict_, name=None):
+    if name is None:
+        # If name not provided, maybe it's provided in the dictionary
+        name = dict_.pop("class", None)
+
+    keys = set(dict_.keys())
+    if len(keys ^ {"x1", "x2", "y1", "y2"}) == 0:
+        return Line(name=name, **dict_)
+    elif len(keys ^ {"x", "y"}) == 0:
+        return Point(name=name, **dict_)
+    elif len(keys ^ {"x", "y", "width", "height"}) == 0:
+        return Rectangle(name=name, **dict_)
+
+
+class Feature:
+
+    def __init__(self, name, schema):
+        self.name = name
+        self.schema = schema
+
+BALL = Feature("Ball", Rectangle)
+GOAL_POST = Feature("Goal Post", Rectangle)
+FIELD_LINE = Feature("Field Line", Line)
+CORNER = Feature("Corner", Point)
+T_JUNCTION = Feature("T Junction", Point)
+X_JUNCTION = Feature("X Junction", Point)
+PENALTY_SPOT = Feature("Penalty Spot", Point)
+CENTER_CIRCLE = Feature("Center Circle", Point)
+NAO = Feature("Nao", Rectangle)
+NOT_BALL = Feature("Not Ball", Rectangle)
+
+USED_FEATURES = [BALL, GOAL_POST, FIELD_LINE, CORNER, T_JUNCTION, X_JUNCTION,
+                 PENALTY_SPOT, CENTER_CIRCLE, NAO]
