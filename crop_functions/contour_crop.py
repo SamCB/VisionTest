33936c5ed40dc92979c82e731cfc365d7d64ddcd
Sam Collis-Bird <s.cbird01@gmail.com>
2016-10-09 12:46:20 +1100

    Created function, but evident not the right method to use.

--- crop_functions/contour_crop.py
+++ crop_functions/contour_crop.py
@@ -1,7 +1,10 @@
 import cv2
 
 def retrieve_subsections(img):
-    contours, _ = cv2.findContours(img, cv2.RETR_LIST, cv2.CHAIN_APPROX_NONE)
+    #img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+    edges = cv2.Canny(img, 100, 200)
+    cv2.imshow("edges", edges)
+    contours, _ = cv2.findContours(edges, cv2.RETR_LIST, cv2.CHAIN_APPROX_NONE)
     for contour in contours:
         # x, y, w, h
         yield cv2.boundingRect(contour)
