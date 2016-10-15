a50addd3d0e02f5d592bda4b5ee6a3c0ac80b814
Sam Collis-Bird <s.cbird01@gmail.com>
2016-10-15 15:07:42 +1100

    Getting started changing args

--- main.py
+++ main.py
@@ -77,13 +77,13 @@ def main(function, function_args,
         # If we want to save the cropped images, save them.
         if save_img:
             for name, points in results:
-                cropped = img[points['y']:points['y']+points['height'], 
+                cropped = img[points['y']:points['y']+points['height'],
                                         points['x']:points['x']+points['width']]
-                cv2.imwrite(os.path.join('cropped_ims', 
+                cv2.imwrite(os.path.join('cropped_ims',
                                          '0'*(6-int(np.log10(im_count))) +
                                                str(im_count)) + '.bmp', cropped)
                 im_count += 1
-            
+
         # If we want to display the image, display it
         if show_img:
             for name, points in results:
@@ -105,20 +105,22 @@ Test out vision functions for Robot Soccer.
 Examples:
 ---------------------------
 
+---- Command Line Setup ----
+
 To run some example function through the
 camera input:
 
-    python {name} function.py camera.py
+    python {name} cl function.py camera.py
 
 To run some random function with an example
 input and the example annotation test:
 
-    python {name} function.py other_input.py annotation.py
+    python {name} cl function.py other_input.py annotation.py
 
 If a camera requires input, for example,
 the name of video files, you can use:
 
-    python {name} function.py video.py annotation.py -i foo.mp4 -i bar.mp4 -i baz.mp4
+    python {name} cl function.py video.py annotation.py -i foo.mp4 -i bar.mp4 -i baz.mp4
 
 Where each -i argument will be handed as a
 seperate argument to the initialiser in
@@ -127,16 +129,63 @@ video.py.
 Similarly, use -f to hand arguments into
 the function initialiser and -a to hand
 arguments into the annotation initialiser
+
+---- Setup File ----
+
+Alternatively, save your setup in setup.json like so:
+
+    {{
+        "setup_name": {{
+            "function": ["function.py", "any", "other", "args"],
+            "input": ["input.py", "more arguments"],
+            "annotations": "can_be_called_as_string_with_no_args.py"
+        }},
+        "another_setup": {{
+            ...
+        }},
+        ...
+    }}
+
+And call the function with:
+
+    python {name} sf setup.json setup_name
+
+For quick, easy usage.
 """.format(name=__file__)
     parser = argparse.ArgumentParser(description=description, epilog=epilog,
                                      formatter_class=argparse.RawDescriptionHelpFormatter)
+
+    #
+    # General arguments
+    #
     parser.add_argument(
         '-s', '--silent', action='store_true',
         help='do not display images during testing')
     parser.add_argument(
         '-v', '--save', dest='save', action='store_true',
         help='save cropped images during testing')
-    function_group = parser.add_argument_group('function group')
+
+    subparser = parser.add_subparsers(
+        title='setup method',
+        description='should we use setup files or the command line for setup?'
+    )
+    #
+    # Arguments for using setup.json
+    #
+    setup_file = subparser.add_parser('sf', help='use the setup file')
+    setup_file.add_argument(
+        "setup_file", help="json file containing setup method we want"
+    )
+    setup_file.add_argument(
+        "setup_method", help="method in the setup_file to use"
+    )
+
+    #
+    # Arguments for using command line setup
+    #
+    command_line_group = subparser.add_parser('cl', help='use the command line')
+
+    function_group = command_line_group.add_argument_group('function group')
     function_group.add_argument(
         "function", help="module containing method for performing CV analysis"
     )
@@ -145,7 +194,7 @@ arguments into the annotation initialiser
         help="arguments to pass to the function module initialiser"
     )
 
-    input_group = parser.add_argument_group('input group')
+    input_group = command_line_group.add_argument_group('input group')
     input_group.add_argument(
         "input", help="module that provides image frames for the analysis"
     )
@@ -154,7 +203,7 @@ arguments into the annotation initialiser
         help="arguments to pass to the input module initialiser"
     )
 
-    annotations_group = parser.add_argument_group('annotations group')
+    annotations_group = command_line_group.add_argument_group('annotations group')
     annotations_group.add_argument(
         "annotations", nargs="?", default=None,
         help="module returning the correct annotations for given images"
@@ -163,9 +212,11 @@ arguments into the annotation initialiser
         "-a", "--aarg", action="append", default=[],
         help="arguments to pass to the annotations module initialiser"
     )
+
     args = parser.parse_args()
-    
-    main(args.function, args.farg,
-         args.input, args.iarg,
-         args.annotations, args.aarg,
-         vars(args))
+
+    print(args)
+    # main(args.function, args.farg,
+    #      args.input, args.iarg,
+    #      args.annotations, args.aarg,
+    #      vars(args))
